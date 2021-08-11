d2f2cf25ed936f56c985ae01c2ce3fef659f5cc8
ahkamul <sasraishougat@gmail.com>
2021-08-11 09:43:41 +0600

    index.js file created based on class # 51 & 53 (#52 not found)

---
+++ index.js
@@ -0,0 +1,41 @@
+// Class 051: JS Object Literal vs Constructor
+
+// var obj = {}
+
+// console.log(obj)
+
+// var point = {
+//     x : 10,
+//     y : 20
+// }
+// //console.log(point)
+
+// point.y = 30
+
+// //console.log(point)
+
+// point.z = 30
+
+// console.log(point)
+
+
+// Class 053: JS Accessing Object Properties in Javascript 
+
+// A. Dot Notation
+
+var point = {
+    x : 10,
+    y : 20,
+    z : 15
+}
+
+// console.log(point.x)
+// console.log(point.y)
+// console.log(point.z)
+// console.log(point.x + point.y)
+
+
+// B. Array Notation
+
+console.log((point['x']))
+console.log((point['x'] + point['z']))
